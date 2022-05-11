d0dedf4e3d54f6b0e0b6d4dc6c8c57ccc4bebd24
renanbehr <92093888+renanbehr@users.noreply.github.com>
2022-05-11 09:48:15 -0300

    Add files via upload

---
+++ Aula2Unidade1-Ex003.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    float valor1=0, valor2=0;
+    printf("\n Digite o primeiro valor: ");
+    scanf("%f",&valor1);
+    printf("\n Digite o segundo valor: ");
+    scanf("%f",&valor2);
+    printf("\n Variável 1 = %.2f",valor1);
+    printf("\n Variável 2 = %.2f \n",valor2);
+    return 0;
+}
