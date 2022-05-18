6d53ad0b9425eb619ce418ed225f402cc61c4e90
renanbehr <92093888+renanbehr@users.noreply.github.com>
2022-05-18 11:43:23 -0300

    Create D2U1A1-EX01-Função simples 1.c

---
+++ D2U1A1-EX01-Função simples 1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int somar(){
+    return 2 + 3;
+}
+int main(){
+   int resultado = 0;
+   resultado = somar();
+   printf("O resultado da função é = %d\n", resultado);
+    return 0;
+}
